2e05d1b11a86b66149eb7aa2dbaaaeb9b427eda1
hz639 <hz639@cornell.edu>
2024-10-26 14:40:19 -0400

    Debugging

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-open Graphics
-
-let () =
-  open_graph " 800x600";
-  set_window_title "game start";
-  let _ = read_key () in
-  close_graph ()
-=======
-<<<<<<< HEAD
-(* simple_window.ml *)
-
 (* Open the Graphics module *)
 open Graphics
 
@@ -35,28 +23,3 @@ let () =
   (* Wait for a mouse click before closing *)
   let _ = read_key () in
   close_graph () (* Close the window *)
-=======
-<<<<<<< HEAD
-let () = print_endline "Starting main function..."
-=======
-let main () =
-  (* Initialize the Gtk library *)
-  let _ = GMain.init () in
-
-  (* Create the main window *)
-  let window =
-    GWindow.window ~title:"Simple Window" ~border_width:10 ~width:400
-      ~height:200 ()
-  in
-
-  (* Show the window *)
-  window#show ();
-
-  (* Start the GTK main loop *)
-  GMain.main ()
-
-(* Run the main function *)
-let () = main ()
->>>>>>> refs/remotes/origin/main
->>>>>>> refs/remotes/origin/main
->>>>>>> refs/remotes/origin/main
