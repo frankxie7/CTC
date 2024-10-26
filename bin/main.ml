5786fa5a9672d5e8e69e292accd105e2999c46ce
Frank Xie <fx58@cornell.edu>
2024-10-26 14:30:57 -0400

    Main function

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,27 @@
-let () = print_endline "Hello, World! word"
+(* simple_window.ml *)
+
+(* Open the Graphics module *)
+open Graphics
+
+let () =
+  (* Initialize the graphics system *)
+  open_graph " 640x480";
+
+  (* Create a window of size 640x480 *)
+
+  (* Set the background color *)
+  set_window_title "Simple Window";
+  set_color black;
+  fill_rect 0 0 (size_x ()) (size_y ());
+
+  (* Fill the window with black color *)
+
+  (* Draw some text *)
+  set_color white;
+  moveto ((size_x () / 2) - 50) (size_y () / 2);
+  (* Center the text *)
+  draw_string "Hello, OCaml Graphics!";
+
+  (* Wait for a mouse click before closing *)
+  let _ = read_key () in
+  close_graph () (* Close the window *)
