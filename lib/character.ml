3f37aa4d00a4c5fa44dabf2d72b2a51c9f456217
Alexander Jankowich <aej56@cornell.edu>
2024-10-28 13:21:35 -0400

    Added a deck of cards for camel1 alont with documenting each .mli file
    -Deck added within  character.ml
    -Spec added t oall .mli

--- lib/character.ml
+++ lib/character.ml
@@ -1,3 +1,6 @@
+open Deck
+open Card
+
 type t = {
   hp : int;
   energy : int;
@@ -5,3 +8,18 @@ type t = {
 }
 
 let camel1 : t = { hp = 80; energy = 3; status = "None" }
+
+let camel1A =
+  [
+    Card.basicA;
+    Card.basicA;
+    Card.basicA;
+    Card.basicA;
+    Card.basicD;
+    Card.basicD;
+    Card.basicD;
+    Card.basicD;
+    Card.basicAD;
+  ]
+
+let camel1_deck = Deck.empty |> List.fold_right Deck.push camel1A
