0f2b498c8abe584b90756d124cc1246e9b891f6e
Frank Xie <fx58@cornell.edu>
2024-12-12 00:37:03 -0500

    Add mli file for enemy.ml

---
+++ lib/enemy.mli
@@ -0,0 +1,65 @@
+open Tsdl.Sdl
+open Tsdl_image
+
+type m = {
+  name : string;
+  damage : int;
+  defend : int;
+  effect : string;
+}
+
+type t = {
+  mutable hp : int;
+  moves : m list;
+  mutable animation : string;
+}
+
+val get_name : m -> string
+(** [get_name m] returns the name of the move [m]. *)
+
+val get_dmg : m -> int
+(** [get_dmg m] returns the damage value of the move [m]. *)
+
+val create_move : string -> int -> int -> string -> m
+(** [create_move n dmg def eff] creates a move with name [n], damage [dmg],
+    defense [def], and effect [eff]. *)
+
+val snake_moves : m list
+(** A list of moves for the snake enemy. *)
+
+val bear_moves : m list
+(** A list of moves for the bear enemy. *)
+
+val man_moves : m list
+(** A list of moves for the human enemy. *)
+
+val create_enemy : int -> m list -> t
+(** [create_enemy h m] creates an enemy with health [h] and a list of moves [m]. *)
+
+val init_snake : unit -> t
+(** [init_snake ()] initializes a snake enemy. *)
+
+val init_bear : unit -> t
+(** [init_bear ()] initializes a bear enemy. *)
+
+val init_man : unit -> t
+(** [init_man ()] initializes a human enemy. *)
+
+val get_hp : t -> int
+(** [get_hp t] returns the current health points of the enemy [t]. *)
+
+val get_moves : t -> m list
+(** [get_moves t] returns the list of moves of the enemy [t]. *)
+
+val get_animation : t -> string
+(** [get_animation t] returns the current animation state of the enemy [t]. *)
+
+val update_animation : t -> string -> unit
+(** [update_animation t x] updates the animation state of the enemy [t] to [x]. *)
+
+val update_hp : t -> int -> unit
+(** [update_hp t x] reduces the health points of the enemy [t] by [x]. *)
+
+val draw_enemy_base : renderer -> texture -> unit
+(** [draw_enemy_base r t] renders the enemy's base on the renderer [r] using the
+    texture [t]. Raises an exception if rendering fails. *)
